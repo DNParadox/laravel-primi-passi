003cd3630fb8ea933abc3642cdbd117eada6c0b9
DNParadox <m96francesco@gmail.com>
2022-08-23 16:14:43 +0200

    Hello dynamic Laravel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel Home</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
@@ -64,6 +64,7 @@
         </style>
     </head>
     <body>
-      Hello Laravel
+      <div> Nome: {{ $name }}</div>
+      <div> Cognome: {{ $lastname }}</div>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $data = [
+        'name' => 'Pippo',
+        'lastname' => 'Baudo',
+
+    ];
+
+    return view('home', $data);
+
+})->name('home');
